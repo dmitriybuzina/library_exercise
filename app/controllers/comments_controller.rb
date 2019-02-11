c078893b2cb8081da0274fd62abbbf8c28f73f7d
dmitriybuzina <dmitry.buzina@faceit-team.com>
2019-02-11 18:26:54 +0200

    views book

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :set_book, only: [ :index, :create ]
+
+  def index
+    @comments = @book.comments.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @book.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    redirect_to @book if @comment.save
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :book_id)
+  end
+
+end
