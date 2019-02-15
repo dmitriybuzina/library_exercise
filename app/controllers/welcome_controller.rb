6f23a7ac6e78f33bba7582575e6e8be69b9c0a5e
dmitriybuzina <dmitry.buzina@faceit-team.com>
2019-02-15 17:36:16 +0200

    like && welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  before_action :top_books
+
+  def index
+    @books = Book.all
+  end
+
+  protected
+  def top_books
+    @top_books = Book.order('counter Desc').limit(5)
+  end
+
+end
